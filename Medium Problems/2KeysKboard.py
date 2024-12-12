55079420bc6af6a3e32696f98d4058efd128ff2e
Gatimoro <trixex.lol@gmail.com>
2024-12-12 17:01:16 +0200

    Update 2KeysKboard.py
    corrected a redundant line

--- Medium Problems/2KeysKboard.py
+++ Medium Problems/2KeysKboard.py
@@ -1,10 +1,10 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        i,ans=2,-1
-        while n>1:#if we have a starting number Strt and we want to get to Prime*Strt, we need to apply #Prime operations. If we have the prime factorization of the number we can simply add the primes and correct with a -1 to directly get the answer.
+        i,ans=2,0
+        while n>1:#if we have a starting number Strt and we want to get to Prime*Strt, we need to apply #Prime operations. If we have the prime factorization of the number we can simply add the primes.
             if not n%i:
                 n//=i
                 ans+=i
                 continue
             i+=1
-        return ans+n
+        return ans
