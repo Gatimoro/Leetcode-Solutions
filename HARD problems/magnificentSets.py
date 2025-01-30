6dbc90c2bd26b85421c0c80deb9736c938b14f5c
Gatimoro <trixex.lol@gmail.com>
2025-01-31 00:17:45 +0200

    Update magnificentSets.py

--- HARD problems/magnificentSets.py
+++ HARD problems/magnificentSets.py
@@ -1,7 +1,16 @@
+"""You are given a positive integer n representing the number of nodes in an undirected graph. The nodes are labeled from 1 to n.
+
+You are also given a 2D integer array edges, where edges[i] = [ai, bi] indicates that there is a bidirectional edge between nodes ai and bi. Notice that the given graph may be disconnected.
+
+Divide the nodes of the graph into m groups (1-indexed) such that:
+
+Each node in the graph belongs to exactly one group.
+For every pair of nodes in the graph that are connected by an edge [ai, bi], if ai belongs to the group with index x, and bi belongs to the group with index y, then |y - x| = 1.
+Return the maximum number of groups (i.e., maximum m) into which you can divide the nodes. Return -1 if it is impossible to group the nodes with the given conditions."""
 
 
 
-class Solution:"""I am super glad of this solution. I didn't solve it immediately but I learned a great amount of stuff along the way"""
+class Solution:"""I am super pround of this solution. I didn't solve it immediately but I learned a great amount of stuff along the way"""
     def magnificentSets(self, n: int, edges: List[List[int]]) -> int:
         joints = [[] for _ in range(n+1)]#adjacency array
         for a, b in edges:
