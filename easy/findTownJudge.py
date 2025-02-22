3c828f2d8c6a343926bcaea35b2687d9b0dd5e89
Gatimoro <trixex.lol@gmail.com>
2025-02-22 21:51:45 +0200

    Update findTownJudge.py

--- easy/findTownJudge.py
+++ easy/findTownJudge.py
@@ -3,19 +3,13 @@ class Solution:
         if n==1 and len(trust)==0:return 1
         votes = [0] * (n+1)
         valid = [True] * (n+1)
-        judges = []
-        judge = None
         for v, t in trust:
             valid[v] = False
             votes[t]+=1
-            if votes[t] == n-1:
-                judges.append(t)
-        for candidate in judges:
-            if valid[candidate]:
-                if not judge:
-                    judge = candidate
-                else: return -1
-        return judge if judge else -1
+        for candidate in range(1,n+1):
+            if valid[candidate] and votes[candidate]==n-1:
+                return candidate
+        return -1
 """In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
 
 If the town judge exists, then:
