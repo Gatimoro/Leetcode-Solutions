6c103c2a7316ce0f53d30009c47698dd00eb45dc
Gatimoro <trixex.lol@gmail.com>
2025-02-22 21:46:09 +0200

    Create keysAndRooms.py

---
+++ Medium Problems/keysAndRooms.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = [False] * len(rooms)
+        visited[0] = True
+        stash = deque([0])
+        while stash:
+            curr = stash.pop()
+            for key in rooms[curr]:
+                if not visited[key]:
+                    stash.append(key)
+                    visited[key] = True
+        return all(visited)
+"""There are n rooms labeled from 0 to n - 1 and all the rooms are locked except for room 0. Your goal is to visit all the rooms. However, you cannot enter a locked room without having its key.
+
+When you visit a room, you may find a set of distinct keys in it. Each key has a number on it, denoting which room it unlocks, and you can take all of them with you to unlock the other rooms.
+
+Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i, return true if you can visit all the rooms, or false otherwise."""
