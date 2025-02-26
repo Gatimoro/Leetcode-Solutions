d92caca94d9ed1d1d345e2058a118c33d19f0269
Gatimoro <trixex.lol@gmail.com>
2025-02-26 23:57:15 +0200

    Create minReorder.py

---
+++ Medium Problems/minReorder.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        norm = [[] for _ in range(n)]
+        for s, e in connections:
+            norm[s].append((e,1))
+            norm[e].append((s,0))
+        def count(daddy, city):
+            inverses = 0
+            for neighbor, d in norm[city]:
+                if neighbor != daddy:
+                    inverses += d + count(city, neighbor)
+            return inverses
+        return count(-1,0)
+"""There are n cities numbered from 0 to n - 1 and n - 1 roads such that there is only one way to travel between two different cities (this network form a tree). Last year, The ministry of transport decided to orient the roads in one direction because they are too narrow.
+
+Roads are represented by connections where connections[i] = [ai, bi] represents a road from city ai to city bi.
+
+This year, there will be a big event in the capital (city 0), and many people want to travel to this city.
+
+Your task consists of reorienting some roads such that each city can visit the city 0. Return the minimum number of edges changed.
+
+It's guaranteed that each city can reach city 0 after reorder."""
