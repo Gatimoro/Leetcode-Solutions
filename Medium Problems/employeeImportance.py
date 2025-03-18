d41a628bc7309fbde1262c94701641d5583d83d9
Gatimoro <trixex.lol@gmail.com>
2025-03-19 00:13:00 +0200

    Create employeeImportance.py

---
+++ Medium Problems/employeeImportance.py
@@ -0,0 +1,26 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        getByID = dict()
+        for employee in employees:
+            getByID[employee.id] = employee
+        def importance(ID):
+            emp = getByID[ID]
+            return emp.importance + sum(importance(sub) for sub in emp.subordinates)
+        return importance(id)
+"""You have a data structure of employee information, including the employee's unique ID, importance value, and direct subordinates' IDs.
+
+You are given an array of employees employees where:
+
+employees[i].id is the ID of the ith employee.
+employees[i].importance is the importance value of the ith employee.
+employees[i].subordinates is a list of the IDs of the direct subordinates of the ith employee.
+Given an integer id that represents an employee's ID, return the total importance value of this employee and all their direct and indirect subordinates."""
