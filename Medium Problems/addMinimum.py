fa429d90a46e954ef9d834aab5d599017c1b4b1b
Gatimoro <trixex.lol@gmail.com>
2025-11-17 23:13:12 +0100

    Create addMinimum.py

---
+++ Medium Problems/addMinimum.py
@@ -0,0 +1,22 @@
+# Given a string word to which you can insert letters "a", "b" or "c" anywhere and any number of times, return the minimum number of letters that must be inserted so that word becomes valid.
+
+# A string is called valid if it can be formed by concatenating the string "abc" several times.
+class Solution:
+    def addMinimum(self, word: str) -> int:
+        abc = ['a','b','c']
+        ads = 0
+        abi = 0
+        i = 0
+        while i < len(word):
+            l = word[i]
+            if l == abc[abi]:
+                i += 1
+            else:
+                ads += 1
+            abi += 1
+            abi %= 3
+        if word[-1] == 'a':
+            return ads + 2
+        elif word[-1] == 'b':
+            return ads + 1
+        return ads
