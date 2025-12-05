d480bb1e38e04c8f2c5a6bc41e35f440fa230b4d
Gatimoro <trixex.lol@gmail.com>
2025-12-05 19:51:19 +0100

    Update countPartitions.go

--- Medium Problems/countPartitions.go
+++ Medium Problems/countPartitions.go
@@ -1,10 +1,4 @@
 func countPartitions(nums []int) int {
-    guess := 1.0
-    x:= 2.0
-	for prev_guess :=0.0; guess != prev_guess; guess = guess - (guess*guess - x) / (2*guess){
-		prev_guess = guess;
-		fmt.Println(guess, prev_guess)
-	}
     is_odd := 0
     for _, n := range nums {
         is_odd ^= n % 2
