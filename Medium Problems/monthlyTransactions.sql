743c51bf78b177e9a326f4823903b09f45bfbf39
Gatimoro <trixex.lol@gmail.com>
2025-12-07 10:55:53 +0100

    Create monthlyTransactions.sql

---
+++ Medium Problems/monthlyTransactions.sql
@@ -0,0 +1,24 @@
+-- Example 1:
+
+-- Input: 
+-- Transactions table:
+-- +------+---------+----------+--------+------------+
+-- | id   | country | state    | amount | trans_date |
+-- +------+---------+----------+--------+------------+
+-- | 121  | US      | approved | 1000   | 2018-12-18 |
+-- | 122  | US      | declined | 2000   | 2018-12-19 |
+-- | 123  | US      | approved | 2000   | 2019-01-01 |
+-- | 124  | DE      | approved | 2000   | 2019-01-07 |
+-- +------+---------+----------+--------+------------+
+-- Output: 
+-- +----------+---------+-------------+----------------+--------------------+-----------------------+
+-- | month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
+-- +----------+---------+-------------+----------------+--------------------+-----------------------+
+-- | 2018-12  | US      | 2           | 1              | 3000               | 1000                  |
+-- | 2019-01  | US      | 1           | 1              | 2000               | 2000                  |
+-- | 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
+-- +----------+---------+-------------+----------------+--------------------+-----------------------+
+# Write your MySQL query statement below
+select date_format(trans_date, '%Y-%m' ) as month, country, coalesce(count(state), 0) as trans_count, coalesce(count(case when state = "approved"then 1 end),0) as approved_count, coalesce(sum(amount), 0) as trans_total_amount,sum(case when state = "approved" then amount else 0 end) as approved_total_amount
+from Transactions
+group by month, country
