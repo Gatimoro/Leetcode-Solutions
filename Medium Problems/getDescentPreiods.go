88696cf7bfaec4217b93e221a262458aaee58aac
Gatimoro <trixex.lol@gmail.com>
2025-12-15 01:45:53 +0100

    Create getDescentPreiods.go

---
+++ Medium Problems/getDescentPreiods.go
@@ -0,0 +1,29 @@
+// You are given an integer array prices representing the daily price history of a stock, where prices[i] is the stock price on the ith day.
+
+// A smooth descent period of a stock consists of one or more contiguous days such that the price on each day is lower than the price on the preceding day by exactly 1. The first day of the period is exempted from this rule.
+
+// Return the number of smooth descent periods.
+import "fmt"
+func getDescentPeriods(prices []int) int64 {
+    last := prices[0] + 1
+    var total int64 = 0
+    var cur int64 = 0
+    
+
+    for _, p := range prices{
+        // fmt.Println(total)
+        if last == p + 1{
+            cur++
+        }else{
+            total +=  (cur + 1) * cur / 2
+
+            cur = 1
+        }
+        last = p
+        // fmt.Println(total,"\n")
+
+    }
+    total +=  (cur + 1) * cur / 2
+
+    return total
+}
